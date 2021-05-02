42f79fe832fbdcb7ac29da64414897392183ed01
Tomáš Kout <kout.tk@gmail.com>
2021-05-02 18:08:42 +0200

    Init commit

---
+++ skrypti/KTuery.js
@@ -0,0 +1,9 @@
+$('#bok a').click(function(){
+    $('#bok a').removeClass('tu');
+    $(this).addClass('tu');
+    var cesta = $(this).attr('data-nudle');
+    if(cesta){
+        var preobr = $(this).attr('href').replace('#','');
+        $('#hlava > img').attr('src',cesta+'/'+preobr+'.jpg');
+    }
+});
